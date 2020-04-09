dc19862c1a18ebf2d1cf7b94b9724c43265f7911
sunway513 <peng.sun@amd.com>
2020-04-09 16:19:35 +0000

    remove training on 6 GPU

--- collect-res.sh
+++ collect-res.sh
@@ -1,5 +1,8 @@
+echo "Training on 1 GPU:"
 bash bert-calculate-perf.sh bert-large-perf-1*
+echo "Training on 2 GPU:"
 bash bert-calculate-perf.sh bert-large-perf-2*
+echo "Training on 4 GPU:"
 bash bert-calculate-perf.sh bert-large-perf-4*
-bash bert-calculate-perf.sh bert-large-perf-6*
+echo "Training on 8 GPU:"
 bash bert-calculate-perf.sh bert-large-perf-8*
