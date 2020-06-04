4318b407f5e7a35137db7c40b0577b8ba85ea25a
sunway513 <peng.sun@amd.com>
2020-06-04 23:03:45 +0000

    add data/scripts/configs for master branch

---
+++ scripts/bert-base/collect-res.sh
@@ -0,0 +1,8 @@
+echo "Training on 1 GPU:"
+bash bert-calculate-perf.sh bert-base-perf-1*
+echo "Training on 2 GPU:"
+bash bert-calculate-perf.sh bert-base-perf-2*
+echo "Training on 4 GPU:"
+bash bert-calculate-perf.sh bert-base-perf-4*
+echo "Training on 8 GPU:"
+bash bert-calculate-perf.sh bert-base-perf-8*
---
+++ scripts/bert-base/run-bert-hvd.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+export HSA_FORCE_FINE_GRAIN_PCIE=1
+export NCCL_DEBUG=INFO
+
+NUM_GPUS=${NUM_GPUS:-1}
+MODEL_NAME=${MODEL_NAME:-bert_base}
+
+# remove the previous checkpoints
+rm -rf /tmp/pretraining_output/*
+
+cd ../..
+
+mpirun -np ${NUM_GPUS} \
+    -H localhost:${NUM_GPUS} \
+    -bind-to none -map-by slot \
+    -x NCCL_DEBUG=INFO -x LD_LIBRARY_PATH -x PATH \
+    -mca pml ob1 -mca btl ^openib \
+    python3 run_pretraining.py \
+    --input_file=./bert-data/*.tfrecord \
+    --output_dir=/tmp/pretraining_output \
+    --do_train=True \
+    --do_eval=False \
+    --use_horovod=True \
+    --bert_config_file=./configs/${MODEL_NAME}/bert_config.json \
+    --train_batch_size=20 \
+    --max_seq_length=512 \
+    --max_predictions_per_seq=20 \
+    --num_train_steps=$((20*NUM_GPUS)) \
+    --num_warmup_steps=10 \
+    --learning_rate=2e-5 |& tee bert-base-perf-${NUM_GPUS}gpus.txt
+
+# remove the generated checkpoints
+rm -rf /tmp/pretraining_output/*
+
+# calculate and print out per GPU performance
+./bert-calculate-perf.sh bert-base-perf-${NUM_GPUS}gpus.txt
